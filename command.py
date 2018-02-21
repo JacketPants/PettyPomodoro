a1d1ee414332867167d02415886792f6e6186828
Joe <joezhang@outlook.com>
2018-02-21 22:56:28 +0800

    write base code

--- command.py
+++ command.py
@@ -2,3 +2,29 @@ import sys
 import pettypomodoro
 
 PP = pettypomodoro.PettyPomodoro()
+
+Info = '''
+PettyPomodoro - 0.1.1
+---------
+'''
+
+
+def PrintInfo():
+    print(Info)
+
+
+if __name__ == "__main__":
+    PrintInfo()
+    while True:
+        command = input().lower()
+        if command == 'start' or command == 'next':
+            pass
+        elif command == 'stop' or command == 'pause':
+            pass
+        elif command == 'exit':
+            break
+        elif command == 'help':
+            pass
+        else:
+            pass
+    sys.exit()
