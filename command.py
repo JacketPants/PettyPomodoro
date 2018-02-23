1e72c1a20948b1904632362aa9eeaa02bb86dcbb
Joe <joezhang@outlook.com>
2018-02-23 22:46:19 +0800

    updata 0.2.23

--- command.py
+++ command.py
@@ -1,26 +1,55 @@
 import sys
+# import os
+import threading
+import time
 import pettypomodoro
+import getpass
 
-PP = pettypomodoro.PettyPomodoro()
+pp = pettypomodoro.PettyPomodoro()
 
 Info = '''
-PettyPomodoro - 0.1.1
+PettyPomodoro - 0.2.23
 ---------
 '''
+
+
+def Timer():
+    isPlayAudio = 0
+    while True:
+        PrintState()
+        if pp.GetState == "RUN" or pp.GetState == "REST":
+            isPlayAudio = 1
+        elif isPlayAudio == 1:
+            PlayAudio()
+            isPlayAudio = 0
+        time.sleep(0.5)
 
 
 def PrintInfo():
     print(Info)
 
 
+def PrintState():
+    state = pp.GetState()
+    print('\r', state, int(pp.GetTaskTime()) if state !=
+          "STOP" else '', end='', flush=True)
+
+
+def PlayAudio():
+    print('\a', end='', flush=True)
+
+
 if __name__ == "__main__":
     PrintInfo()
+    (threading.Thread(target=Timer)).start()
+    PlayAudio()
     while True:
-        command = input().lower()
+        command = getpass.getpass(prompt='')
         if command == 'start' or command == 'next':
-            pass
+            if pp.GetState != "RUN" or pp.GetState != "REST":
+                pp.StartNext()
         elif command == 'stop' or command == 'pause':
-            pass
+            pp.Stop()
         elif command == 'exit':
             break
         elif command == 'help':
