95bf6b871f8b57d8a7a171ec84f160aa7cdf588b
Joe <joezhang@outlook.com>
2018-02-23 23:07:39 +0800

    fix bug

--- command.py
+++ command.py
@@ -1,14 +1,14 @@
+import getpass
 import sys
-# import os
 import threading
 import time
-import pettypomodoro
-import getpass
 
-pp = pettypomodoro.PettyPomodoro()
+import pettypomodoro
+
+pp = pettypomodoro.PettyPomodoro(25, 5, 15, 0.5)
 
 Info = '''
-PettyPomodoro - 0.2.23
+PettyPomodoro - 0.2.27
 ---------
 '''
 
@@ -17,12 +17,17 @@ def Timer():
     isPlayAudio = 0
     while True:
         PrintState()
-        if pp.GetState == "RUN" or pp.GetState == "REST":
+        state = pp.GetState()
+        if state == "RUN" or state == "REST":
             isPlayAudio = 1
         elif isPlayAudio == 1:
             PlayAudio()
             isPlayAudio = 0
         time.sleep(0.5)
+
+
+def PlayAudio():
+    print('\a', end='', flush=True)
 
 
 def PrintInfo():
@@ -31,23 +36,17 @@ def PrintInfo():
 
 def PrintState():
     state = pp.GetState()
-    print('\r', state, int(pp.GetTaskTime()) if state !=
-          "STOP" else '', end='', flush=True)
-
-
-def PlayAudio():
-    print('\a', end='', flush=True)
+    print('\r', state, int(pp.GetTaskTime()) if state ==
+          "RUN" or state == "REST" else '', end='\t\t\t', flush=True)
 
 
 if __name__ == "__main__":
     PrintInfo()
     (threading.Thread(target=Timer)).start()
-    PlayAudio()
     while True:
         command = getpass.getpass(prompt='')
         if command == 'start' or command == 'next':
-            if pp.GetState != "RUN" or pp.GetState != "REST":
-                pp.StartNext()
+            pp.StartNext()
         elif command == 'stop' or command == 'pause':
             pp.Stop()
         elif command == 'exit':
